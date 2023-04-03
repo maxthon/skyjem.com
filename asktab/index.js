d5ac00b62721dc82f47f3769df36e2f2d124502c
bloodchen <31466551+bloodchen@users.noreply.github.com>
2023-04-03 01:09:27 +0000

    update

--- asktab/index.js
+++ asktab/index.js
@@ -1,13 +1,7 @@
-'use strict';
-var input = document.getElementById('input'),
-    btn = document.getElementById('btn');
-function goBing() {
+
+function onSearch() {
+    var input = document.querySelector('#search-term')
     var n = input.value.trim();
     window.open('https://www.bing.com/search?q='.concat(n));
 }
-input.addEventListener('keydown', function (n) {
-    'Enter' === n.key && goBing();
-}),
-    btn.addEventListener('click', function (n) {
-        goBing();
-    });
+
