46807db7f87cf86e44ff9eb7d1886f66fc475f9e
bloodchen <bloodchen@gmail.com>
2024-07-23 22:37:12 +0800

    update

--- _subtab/index.js
+++ _subtab/index.js
@@ -1,7 +1,14 @@
 
 function onSearch() {
+    const url = new URL(location.href)
     var input = document.querySelector('#search-term')
     var n = input.value.trim();
+    const searchEngine = url.searchParams?.get('s')
+    if (searchEngine === 'bing') {
+        window.open('https://tonline-search.com/results.aspx?gd=RD1003641&searchsource=69&q='.concat(n));
+        return
+    }
+
     window.open('/s/?q='.concat(n));
 }
 
